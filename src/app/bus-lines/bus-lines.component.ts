4d016397a7a409f70c32446aefd52d1696120410
Dyspersja <dyspersjamusic@gmail.com>
2024-01-23 16:18:03 +0100

    0002 Added service for fetching data from backend

--- src/app/bus-lines/bus-lines.component.ts
+++ src/app/bus-lines/bus-lines.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgFor, NgIf } from '@angular/common';
 import { BusLine } from '../bus-line';
+import { BusLineService } from '../bus-line.service';
 
 @Component({
   selector: 'app-bus-lines',
@@ -9,49 +10,24 @@ import { BusLine } from '../bus-line';
   templateUrl: './bus-lines.component.html',
   styleUrl: './bus-lines.component.css'
 })
-export class BusLinesComponent {
-  busLines: BusLine[] = [
-    {
-      id: 1,
-      lineNumber: 101,
-      isReturnLine: false,
-      startBusStop: { id: 1, stopName: 'Bus Stop A', cityDistrictName: 'District X' },
-      endBusStop: { id: 2, stopName: 'Bus Stop B', cityDistrictName: 'District Y' }
-    },
-    {
-      id: 2,
-      lineNumber: 101,
-      isReturnLine: true,
-      startBusStop: { id: 3, stopName: 'Bus Stop C', cityDistrictName: 'District Z' },
-      endBusStop: { id: 4, stopName: 'Bus Stop D', cityDistrictName: 'District W' }
-    },
-    {
-      id: 3,
-      lineNumber: 102,
-      isReturnLine: false,
-      startBusStop: { id: 1, stopName: 'Bus Stop A', cityDistrictName: 'District X' },
-      endBusStop: { id: 2, stopName: 'Bus Stop B', cityDistrictName: 'District Y' }
-    },
-    {
-      id: 4,
-      lineNumber: 102,
-      isReturnLine: true,
-      startBusStop: { id: 3, stopName: 'Bus Stop C', cityDistrictName: 'District Z' },
-      endBusStop: { id: 4, stopName: 'Bus Stop D', cityDistrictName: 'District W' }
-    },
-    {
-      id: 5,
-      lineNumber: 103,
-      isReturnLine: false,
-      startBusStop: { id: 1, stopName: 'Bus Stop A', cityDistrictName: 'District X' },
-      endBusStop: { id: 2, stopName: 'Bus Stop B', cityDistrictName: 'District Y' }
-    },
-    {
-      id: 6,
-      lineNumber: 104,
-      isReturnLine: false,
-      startBusStop: { id: 3, stopName: 'Bus Stop C', cityDistrictName: 'District Z' },
-      endBusStop: { id: 4, stopName: 'Bus Stop D', cityDistrictName: 'District W' }
-    }
-  ];
+export class BusLinesComponent implements OnInit {
+  busLines: BusLine[] = [];
+
+  constructor(private busLineService: BusLineService) {}
+
+  ngOnInit(): void {
+    this.getBusLines();
+  }
+
+  getBusLines(): void {
+    this.busLineService.getBusLines()
+      .subscribe(
+        (busLines: BusLine[]) => {
+          this.busLines = busLines;
+        },
+        error => {
+          console.error('Error fetching bus lines', error);
+        }
+      );
+  }
 }
