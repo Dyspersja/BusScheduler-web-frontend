1c519e110c9e21c25041d31c0bd159fcebc25d5a
Dyspersja <dyspersjamusic@gmail.com>
2024-01-23 18:53:05 +0100

    0003 Added routing to new bus stops component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { BusLinesComponent } from './bus-lines/bus-lines.component';
+import { BusStopsComponent } from './bus-stops/bus-stops.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/bus_lines', pathMatch: 'full' },
-    { path: 'bus_lines', component: BusLinesComponent }
+    { path: 'bus_lines', component: BusLinesComponent },
+    { path: 'bus_stops/:busLineNumber', component: BusStopsComponent }
 ];
