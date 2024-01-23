2be590cdd7c7a47b0d8ccca3138596f7f3f93adb
Dyspersja <dyspersjamusic@gmail.com>
2024-01-23 17:17:38 +0100

    0003 Made empty path redirect to /bus_lines

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { BusLinesComponent } from './bus-lines/bus-lines.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/bus_lines', pathMatch: 'full' },
     { path: 'bus_lines', component: BusLinesComponent }
 ];
