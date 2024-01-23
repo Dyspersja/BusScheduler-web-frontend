24a06f043c4958186c32a6d6bccc118ff1206a05
Dyspersja <dyspersjamusic@gmail.com>
2024-01-23 14:07:35 +0100

    0002 Added bus lines component

---
+++ src/app/bus-lines/bus-lines.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bus-lines',
+  standalone: true,
+  imports: [],
+  templateUrl: './bus-lines.component.html',
+  styleUrl: './bus-lines.component.css'
+})
+export class BusLinesComponent {
+
+}
