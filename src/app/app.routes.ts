3d481dd1c7cb73c1378608d9333facbd92ef93f2
Dyspersja <dyspersjamusic@gmail.com>
2024-01-23 16:54:11 +0100

    0003 Added routing to bus line list page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { BusLinesComponent } from './bus-lines/bus-lines.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'bus_lines', component: BusLinesComponent }
+];
