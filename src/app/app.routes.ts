93509bdac0f9ec50980b37b5b3d8432292ba78d1
Dyspersja <dyspersjamusic@gmail.com>
2024-01-24 15:43:44 +0100

    0004 Added timetable route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { BusLinesComponent } from './bus-lines/bus-lines.component';
 import { BusStopsComponent } from './bus-stops/bus-stops.component';
+import { TimetableComponent } from './timetable/timetable.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/bus_lines', pathMatch: 'full' },
     { path: 'bus_lines', component: BusLinesComponent },
-    { path: 'bus_stops/:busLineNumber', component: BusStopsComponent }
+    { path: 'bus_stops/:busLineNumber', component: BusStopsComponent },
+    { path: 'timetable/:busLineNumber', component: TimetableComponent }
 ];
