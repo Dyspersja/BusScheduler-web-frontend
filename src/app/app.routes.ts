aa2bf5a3bd84e195cc1729934ee16686401ae9a7
Dyspersja <dyspersjamusic@gmail.com>
2024-01-24 17:19:24 +0100

    0005 Added TicketZone page to routes and menu bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { BusLinesComponent } from './bus-lines/bus-lines.component';
 import { BusStopsComponent } from './bus-stops/bus-stops.component';
 import { TimetableComponent } from './timetable/timetable.component';
+import { TicketZoneComponent } from './ticket-zone/ticket-zone.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/bus_lines', pathMatch: 'full' },
     { path: 'bus_lines', component: BusLinesComponent },
+    { path: 'ticket_zones', component: TicketZoneComponent },
     { path: 'bus_stops/:busLineNumber', component: BusStopsComponent },
     { path: 'timetable/:busLineNumber', component: TimetableComponent }
 ];
